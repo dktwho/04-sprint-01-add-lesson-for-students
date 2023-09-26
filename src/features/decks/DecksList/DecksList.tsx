c6ea43e872fd564edd9c32e21f70a375cf715b52
Alexander Korobov <dktwho@mail.ru>
2023-09-26 19:56:27 +0300

    maped all Decks

--- src/features/decks/DecksList/DecksList.tsx
+++ src/features/decks/DecksList/DecksList.tsx
@@ -4,6 +4,7 @@ import { decksAPI } from '../decks-api.ts'
 import { setDecksAC } from '../decks-reducer.ts'
 import { useAppDispatch, useAppSelector } from '../../../app/store.ts'
 import { selectDecks } from '../decks-selectors.ts'
+import { DeckItem } from './DeckItem/DeckItem.tsx'
 
 
 export const DecksList = () => {
@@ -16,6 +17,8 @@ export const DecksList = () => {
     })
   }, [dispatch])
   return <ul className={s.list}>
-
+    {decks.map(deck => (
+      <DeckItem key={deck.id} deck={deck}/>
+    ))}
   </ul>
 }
